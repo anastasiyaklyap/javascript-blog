938f9641475b5b671654d206a2722f3c72d30887
anastasiyaklyap <anastasiya.klyap@gmail.com>
2023-01-06 11:51:48 +0100

    add active class to last clicked element

--- js/script.js
+++ js/script.js
@@ -15,27 +15,28 @@
 // });
 
 function titleClickHandler(event) {
+	const clickedElement = this;
 	console.log('Link was clicked!');
 	console.log(event);
-	/* remove class 'active' from all article links  */
+	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for (let activeLink of activeLinks) {
 		activeLink.classList.remove('active');
 	}
-	/* add class 'active' to the clicked link */
-
-	/* remove class 'active' from all articles */
+	/* [IN PROGRESS] add class 'active' to the clicked link */
+	clickedElement.classList.add('active');
+	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll('.posts article');
 
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove('active');
 	}
-	/* get 'href' attribute from the clicked link */
+	/* [IN PROGRESS] get 'href' attribute from the clicked link */
 
-	/* find the correct article using the selector (value of 'href' attribute) */
+	/* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
 
-	/* add class 'active' to the correct article */
+	/* [IN PROGRESS] add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
