b1e3cbb2e59e2103fdb0d15115133adc5fd40e6a
anastasiyaklyap <anastasiya.klyap@gmail.com>
2023-01-09 19:13:29 +0100

    add dynamic list functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 'use strict';
-const posts = Array.from(document.querySelectorAll('aside .titles a'));
-
 const articles = Array.from(document.querySelectorAll('.posts article'));
+
+(() => {
+    const titleList = document.querySelector('.titles');
+    titleList.innerHTML = '';
+    const navBar = document.querySelector('.sidebar .list');
+    articles.map((article) => {
+        const title = article.querySelector('.post-title').textContent;
+        const id = article.getAttribute('id');
+        navBar.insertAdjacentHTML(
+            'beforeend',
+            `<li><a href=#${id}><span>${title}</span></li>`
+        );
+    });
+})();
+
+const posts = Array.from(document.querySelectorAll('aside .titles a'));
 posts.map((post) => {
     post.addEventListener('click', (e) => {
         e.preventDefault();
